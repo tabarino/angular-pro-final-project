da54e8b92ded05e5cacae4c957a04b1a3582c11c
tabarino <tabarino@outlook.com>
2020-11-09 23:54:47 +0000

    Setup Install

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Store } from 'store';
+
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [
+        Store
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule {
+}
